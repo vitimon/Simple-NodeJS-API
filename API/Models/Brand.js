aaba069277290581cafbd269362205c915412f53
Your Name <leonardongc@gmail.com>
2022-06-15 01:02:54 -0300

    realizing how dumb was referencing by name instead of _id, i'm tired, going to sleep.

--- API/Models/Brand.js
+++ API/Models/Brand.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const Brand = mongoose.model('Brand',{
     name: String,
-    product: String
+    product: String,
+    category: String
 })
 
 module.exports = Brand
